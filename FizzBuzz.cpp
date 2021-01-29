c63162296efe2f38171156f46f82b7e43ac81633
DouglasK4 <77516421+DouglasK4@users.noreply.github.com>
2021-01-29 20:15:40 -0300

    Update FizzBuzz.cpp

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -6,31 +6,25 @@ Escreva um programa que imprima os números de 1 a 100.
 Mas para múltiplos de três imprima “Fizz” em vez do número 
 e para os múltiplos de cinco imprima “Buzz”. Para números 
 múltiplos de três e cinco, imprima “FizzBuzz”.*/
-void FizzBuzz(int j){
-    while (j<=100){
-    
-        if(((j % 3)==0) && ((j % 5)==0)){
-            printf("fizzBuzz\n");
 
-        }else if((j % 5)==0){
-            printf("Buzz\n");
-
-        }else if((j % 3)==0){
-            printf("Fizz\n");
-
-        }else{
-            printf("%d\n",j); 
-
-        }    
-     j++;
-    }   
-}
 
 int main(){
 
-int i=0;
+for(int i=0;i<100;i++){
 
-FizzBuzz(i);
+    if(((i % 3)==0) && ((i % 5)==0)){
+        printf("fizzBuzz\n");
+    }else if((i % 5)==0){
+        printf("Buzz\n");
+
+    }else if((i % 3)==0){
+        printf("Fizz\n");
+
+    }else{
+        printf("%d\n",i); 
+
+    }    
+}
 
 return 0;
 }
